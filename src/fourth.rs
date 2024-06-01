806eb656de4d9d8472ba104669ea7bb2248f0349
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2024-06-01 08:24:00 +0100

    MEOW-01: double linked list

---
+++ src/fourth.rs
@@ -0,0 +1,14 @@
+use std::{cell::RefCell, rc::Rc};
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    prev: Link<T>,
+    next: Link<T>,
+}
